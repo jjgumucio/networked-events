1b24b65f1753b6848ebbaa47ea607d21a7ff92ee
carlosgl93 <61713861+carlosgl93@users.noreply.github.com>
2020-09-06 20:21:42 -0300

    browser router, nav, home y sign in responsive

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { Layout, Menu } from 'antd';
+import { HomeOutlined, UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+
+import Home from "./Views/Home"
 import './App.css';
+import SignIn from './Views/SignIn';
+import Eventos from './Views/Eventos';
+import CrearEvento from './Views/CrearEvento';
+
+
+const { Header, Content, Footer, Sider } = Layout;
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/**Header */}
+      <Layout className="layout">
+        <Sider
+          breakpoint="md"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="logo">
+            <Link to="/">
+              <HomeOutlined/>
+            </Link>
+            
+          </div>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              <Link to="/signin">Sign In</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              <Link to="/eventos">Eventos disponibles</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              <Link to="/crearEvento">Crear evento</Link>
+            </Menu.Item>
+            <Menu.Item key="4" icon={<UserOutlined />}>
+              <Link to="/users">Buscar usuario</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+
+        <Layout>
+          <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/signin" component={SignIn} />
+                <Route exact path="/eventos" component={Eventos} />
+                <Route exact path="/crearEvento" component={CrearEvento} />
+              </Switch>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+
+
+      </Layout>
+
+
+      {/**footer */}
     </div>
   );
 }
---
+++ src/Views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Css/Home.css"
+import 'antd/dist/antd.css';
+
+
+function Home() {
+    return (
+        
+        <h1>Home</h1>
+    )
+}
+
+export default Home
